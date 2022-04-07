68b893a937d783ce7755ae03153e0dba8189f29f
CestMoi77 <102248291+CestMoi77@users.noreply.github.com>
2022-04-07 12:24:02 +0200

    Add project files.

---
+++ DataAccess/DbAccess/SqlDataAccess.cs
@@ -0,0 +1,23 @@
+﻿using System.Data.SqlClient;
+using Dapper;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+
+namespace DataAccess.DbAccess;
+
+public class SQLDataAcces
+{
+    private readonly IConfiguration _confiq;
+    public SQLDataAcces(IConfiguration confiq)
+    {
+        _confiq = confiq;  
+    }
+
+    public async Task<IEnumerable<T>> LoadData<T, U>(
+        string storedProcedure,
+        U parameters,
+        string connectionId = "Default")
+    {
+        using IDbConnection connection = new SqlConnection(_confiq.GetConnectionString(connectionId));
+    }
+}
---
+++ UserDB/dbo/Tables/User.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[User]
+(
+	[Id] INT NOT NULL PRIMARY KEY identity,
+	[FirstName] nvarchar(50) NOT null,
+	[LastName] nvarchar(50) not null
+)
