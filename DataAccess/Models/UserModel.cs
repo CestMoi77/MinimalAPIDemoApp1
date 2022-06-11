1f31f0d8b5abd313c8440d038f4add3c3b3f1cca
relhamouchi@outlook.com <your@email.com>
2022-06-11 22:49:14 +0200

    wekcomehiero

--- DataAccess/Models/UserModel.cs
+++ DataAccess/Models/UserModel.cs
@@ -10,6 +10,8 @@ internal class UserModel
 {
     public int Id { get; set; }
     public string FirstName { get; set; }   
-    public string LastName { get; set; }    
+    public string LastName { get; set; }   
+    
+
 
 }
