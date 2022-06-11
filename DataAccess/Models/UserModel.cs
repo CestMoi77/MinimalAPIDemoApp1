fb4f9a94791eac409fcf68662e7b59ac6c159e4a
relhamouchi@outlook.com <your@email.com>
2022-06-11 23:02:19 +0200

    Revereded

--- DataAccess/Models/UserModel.cs
+++ DataAccess/Models/UserModel.cs
@@ -10,15 +10,6 @@ internal class UserModel
 {
     public int Id { get; set; }
     public string FirstName { get; set; }   
-    public string LastName { get; set; }   
-    
-
+    public string LastName { get; set; }    
 
 }
-
-
-
-
-
-
-
